19feb8bfaf0cfa7e6ce023ca3df1300878e07373
galbertsson <gustav.albertsson@hotmail.com>
2019-12-29 20:52:01 +0100

    Removed old codebase

--- backend/src/main/java/com/example/DeckNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class DeckNotFoundException extends RuntimeException {
-    public DeckNotFoundException(Long id){
-        super("Cannot access deck " + id);
-    }
-}
--- backend/src/main/java/com/example/repository/DeckRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Deck;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeckRepository extends JpaRepository<Deck, Long> {
-}
